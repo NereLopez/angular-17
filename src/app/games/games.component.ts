17ead003d1519e2516d98c5c78efe9e2c51195e0
Nere Lopez <nlopez@pav-online.com>
2026-03-06 16:22:26 +0100

    lista juegos

--- src/app/games/games.component.ts
+++ src/app/games/games.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-games',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './games.component.html',
   styleUrl: './games.component.css'
 })
 export class GamesComponent {
-
+ games = [
+    {
+      id: 1,
+      name: 'The Legend of Zelda: Breath of the Wild',
+    },
+   {
+      id: 2,
+      name: 'Super Mario Odyssey',
+    },
+    {
+      id: 3,
+      name: 'Red Dead Redemption 2',
+    }
+  ];
 }
