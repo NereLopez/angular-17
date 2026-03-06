bc31c0ecf4be79eee1bf2f62a9f2c2c17e27ed3a
Nere Lopez <nlopez@pav-online.com>
2026-03-06 15:43:25 +0100

    pequeños cambios

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common'; 
 
 @Component({
   selector: 'app-user',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
+  username = 'Nerea';
+  isLoggedIn = true;
 }
